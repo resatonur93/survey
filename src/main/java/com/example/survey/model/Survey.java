4e2d535205cbbf805b856dca400e2959923664df
Reşat Onur Kurt <resatonurkurt@gmail.com>
2021-06-05 12:13:48 +0300

    Date formatı düzenlendi.

--- src/main/java/com/example/survey/model/Survey.java
+++ src/main/java/com/example/survey/model/Survey.java
@@ -1,9 +1,9 @@
 package com.example.survey.model;
 
 import lombok.Data;
-
+import org.springframework.format.annotation.DateTimeFormat;
 import javax.persistence.*;
-import java.util.Date;
+import java.time.LocalDate;
 
 @Entity
 @Data
@@ -12,18 +12,26 @@ public class Survey {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
+
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "surname")
     private String surname;
 
+    @Column(name = "gender")
     private String gender;
 
-    @Temporal(TemporalType.DATE)
-    private Date birthDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Column(name = "birth_day")
+    private LocalDate birthDate;
 
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "pollster_name")
     private String pollsterName;
 
+    @Column(name = "pollster_surname")
     private String pollsterSurname;
 }
